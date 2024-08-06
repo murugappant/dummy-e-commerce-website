66482f886182574c34833a51548cfb4131fdd1bb
murugappant <murugappan.t1410465@gmail.com>
2024-08-06 20:03:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidenav=document.querySelector(".side-navbar")
+function opening(){
+sidenav.style.left="0"
+sidenav.style.display="block"
+
+}
+function closing()
+{
+sidenav.style.left="-60%"
+
+}
